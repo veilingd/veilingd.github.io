cf0f338c4b1906f2e1e08171bf52e9cf555014ec
anwendung <anwendung@protonmail.com>
2020-05-24 11:20:02 +0200

    Remove faulty JS.

--- js/script.js
+++ js/script.js
@@ -20,35 +20,6 @@ var main = function() {
         parent.appendChild(fbIframe);
         parent.removeChild(fbDummy);
     };
-    // 2 click 'like'
-    //defines the default order of the buttons
-	$.fn.socialSharePrivacy.settings.order = ['facebook', 'gplus', 'twitter', 'tumblr', 'reddit'];
-	$.fn.socialSharePrivacy.settings.path_prefix = '../';
-
-    $('#share').socialSharePrivacy({
-		perma_option: false,
-		uri: 'https://www.facebook.com/veiling.distress/',
-		layout: window.innerWidth < 480 ? 'box' : 'line'
-	});
-    
-	$(window).resize(function () {
-		var layout = window.innerWidth < 480 ? 'box' : 'line';
-		var $share = $('#share');
-		if ($share.socialSharePrivacy('options').layout !== layout) {
-			var enabled = $share.socialSharePrivacy('enabled');
-			$share.socialSharePrivacy('destroy').socialSharePrivacy({
-				perma_option: false,
-				uri: 'https://www.facebook.com/veiling.distress/',
-				layout: layout
-			});
-
-			for (var name in enabled) {
-				if (enabled[name]) {
-					$share.socialSharePrivacy('enable', name);
-				}
-			}
-		}
-	});
 };
 	
 // run main function on startup
