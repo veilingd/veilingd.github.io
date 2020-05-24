4d6f78e57a897d9c84fbcaf2365c887b9cf9f4f1
anwendung <anwendung@protonmail.com>
2020-05-24 10:59:41 +0200

    Handle fb like without lib and remove twitter.

--- js/script.js
+++ js/script.js
@@ -10,6 +10,16 @@ var main = function() {
     }
 	$( '#collapse2').removeClass('in');
 
+    let fbDummy = document.querySelector('#fb-dummy-image');
+    fbDummy.onclick = () => {
+        let parent = fbDummy.parentElement;
+        console.log(parent);
+        let fbIframe = document.createElement('iframe');
+        fbIframe.id = 'fb-like-iframe'
+        fbIframe.src = `https://www.facebook.com/plugins/like.php?locale=en_GB&href=https%3A%2F%2Fwww.facebook.com%2Fveiling.distress%2F&send=false&show_faces=false&action=like&colorscheme=light&width=80&height=20&layout=button_count`
+        parent.appendChild(fbIframe);
+        parent.removeChild(fbDummy);
+    };
     // 2 click 'like'
     //defines the default order of the buttons
 	$.fn.socialSharePrivacy.settings.order = ['facebook', 'gplus', 'twitter', 'tumblr', 'reddit'];
