ef3e240a150adf6d242d7c40a5e0a7d59f1206e7
anwendung <anwendung@protonmail.com>
2020-05-24 11:10:48 +0200

    Remove social share stuff and other unused files.

--- backup/js/activateSongTabs.js
+++
@@ -1,8 +0,0 @@
-	//sets first state for tabbed player navigation
-	$('#songTabsButtonList').children().first().addClass('active');
-	$('#songTabsContentList').children().first().addClass('active');
-	$('#songTabsContentList').children().first().addClass('in');
-	//sets id for each tab's content panel
-	$('#songTabsContentList').children().each(function(index) {
-        $(this).attr('id', index);
-    });
